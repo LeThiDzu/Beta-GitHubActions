dde1179af642dcd098467a1658efef860219ab8d
Dulethi <tranhuuhoan6@gmail.com>
2025-01-08 13:58:39 +0700

    add await to login function

--- PageObjects/pages/login.page.ts
+++ PageObjects/pages/login.page.ts
@@ -8,8 +8,8 @@ export default class LoginPage extends BasePage {
     readonly btn_signIn = this.page.locator('#login-button')
 
     async login(username: string, password: string) {
-        this.fill_text(this.txt_username, username)
-        this.fill_text(this.txt_password, password)
-        this.click_selector(this.btn_signIn)
+        await this.fill_text(this.txt_username, username)
+        await this.fill_text(this.txt_password, password)
+        await this.click_selector(this.btn_signIn)
     }
 }
