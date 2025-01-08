1d50896799efae6157de0f794d495d57edac1735
Dulethi <tranhuuhoan6@gmail.com>
2025-01-08 13:22:22 +0700

    add go to page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,7 @@ import { expect, test } from "../PageObjects/pageFixtures";
 
 
 test('Example test', async ({ loginPage, inventoryPage }) => {
+  await loginPage.open('/')
   await loginPage.login(`${process.env.USERNAME}`, `${process.env.PASSWORD}`);
   await inventoryPage.add_first_product_to_cart()
   expect(await inventoryPage.cart_added_product_number.textContent()).toBe('1')
