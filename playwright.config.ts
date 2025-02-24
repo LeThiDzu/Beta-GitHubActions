4ca9b057df162ac88ac88f4d665c4032d58badbe
LeThiDzu <tranhuuhoan6@gmail.com>
2025-02-24 15:30:14 +0700

    Update slack report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,72 +1,46 @@
-import { defineConfig, devices } from '@playwright/test';
+import {defineConfig, devices} from "@playwright/test";
+import generateCustomLayout from "./commons/report-template"
+require('dotenv').config({ path: '.env' });
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: 0,
-  /* Opt out of parallel tests on CI. */
-  workers: 2,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  testDir: "./tests",
   timeout: 0,
+  snapshotPathTemplate: '{arg}{ext}',
   expect: {
-    timeout: 30000
+    timeout: 60000
   },
+  reporter: [
+    ['html'],  // HTML reporter
+    ['junit', {outputFile: 'test-results/junit.xml'}],
+    [
+      "./node_modules/playwright-slack-report/dist/src/SlackReporter.js",
+      {
+        slackWebHookUrl: process.env.SLACK_WEBHOOK_URL,
+        sendResults: "always",
+        layout: generateCustomLayout,
+      },
+    ],
+    ['@estruyf/github-actions-reporter', {
+      title: 'Media Database E2E Tests',
+      useDetails: true,
+      showError: true
+    }],
+  ],
   use: {
-    trace: 'on-first-retry',
-    navigationTimeout: 30000,
-    actionTimeout: 15000,
-    screenshot: 'only-on-failure'
+    headless: true,
+    trace: "on-first-retry",
+    actionTimeout: 30000,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ]
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: {width: 1920, height: 1080},
+      },
+    }
+  ],
 });
