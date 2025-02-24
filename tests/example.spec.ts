a232beca9ff274dc8be7fd99b80612ae9fa69781
Dule <47436559+LeThiDzu@users.noreply.github.com>
2025-02-24 15:01:31 +0700

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,7 @@ import { expect, test } from "../PageObjects/pageFixtures";
 
 test('Example test', async ({ loginPage, inventoryPage }) => {
   await loginPage.open('/')
+  console.log(`LINK: ${process.env.TEST_RUN_LINK}`)
   await loginPage.login(`${process.env.USERNAME}`, `${process.env.PASSWORD}`);
   await inventoryPage.add_first_product_to_cart()
   expect(await inventoryPage.cart_added_product_number.textContent()).toBe('1')
